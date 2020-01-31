f3e1697f49997f77ccd98e3969b101e301908a59
NicoleLC16 <asalwaysnicole@outlook.com>
2020-01-30 18:04:34 -0800

    Done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
 import piggy from '../porco.png'
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
 	return (
 		<div className="navWrapper">
 			<span className="headerText">Hogwarts</span>
 			<div className="TwirlyPig">
 				<img src={piggy} className="App-logo" alt="piggy" />
 			</div>
-			<span className="normalText">A React App for County Fair Hog Fans</span>
+			<span className="normalText">A React App for County Fair Hog Fans</span><br></br>
+			<button onClick={() => props.orderByName(props.hogs)}>Sort By Name</button>
+			<button onClick={() => props.orderByWeight(props.hogs)}>Sort By Weight</button>
+			<button onClick={() => props.greasedFilter(props.hogs)}>Greased?</button>
 		</div>
 	)
 }
